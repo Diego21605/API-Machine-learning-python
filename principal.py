067b96e72959359b2df822b65684db82887670e8
Diego Fernandez <54678330+Diego21605@users.noreply.github.com>
2022-11-15 20:40:21 -0500

    Actualizacion

--- principal.py
+++ principal.py
@@ -4,17 +4,16 @@ from flask_cors import CORS, cross_origin
 import Sistemas_Reglas.principal as sr
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
-# Mysql Connection
+# Mysql Connection¿
 app.config['MYSQL_HOST'] = 'localhost' 
 app.config['MYSQL_USER'] = 'Admin'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = ''
 mysql = MySQL(app)
 
-# settings A partir de ese momento Flask utilizará esta clave para poder cifrar la información de la cookie
-app.secret_key = "mysecretkey"
+
 
 @app.route('/getSistemaReglas/<v_repiratorio>/<v_fatiga>/<v_dolor>/<v_cuello>/<v_debiliad>', methods=['GET'])
 def getSistemaReglas(v_repiratorio, v_fatiga, v_dolor, v_cuello, v_debiliad):
